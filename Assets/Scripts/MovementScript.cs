689848bf71b151c80b801ea2d0211688f923efca
Erwin Kirichner <ekirichner@yahoo.com>
2023-09-19 21:19:34 +0300

    Movement added

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class MovementScript : MonoBehaviour
@@ -10,8 +11,8 @@ public class MovementScript : MonoBehaviour
 
     [Header("Movement Variables")]
     [SerializeField] float MovementSpeed;
-    [Range(0,25f)]
-    [SerializeField] float MaxSpeed;
+    [Range(0f,5f)]
+    [SerializeField] float MaxYSpeed;
 
     [Header("Jumping Variables")]
     [Range(1f,10f)]
@@ -20,13 +21,31 @@ public class MovementScript : MonoBehaviour
     [SerializeField] float LowJumpMultiplier = 2f;
 
     [Header("Dashing Variables")]
-    [Range(1f,5f)]
+    [Range(1f,25f)]
     [SerializeField] float DashDistance;
+    [SerializeField] float DashDuration;
     [SerializeField] float DashCooldown;
     [SerializeField] Color Color;
+    [SerializeField] private bool IsDashing = false;
+    [SerializeField] private bool CanDash = true;
+    Coroutine DashingCoroutine;
+
+    [Header("Wall Slide Variables")]
+    private bool IsWallSliding = false;
+    [SerializeField] private float WallSlidingSpeed;
+    [SerializeField] private LayerMask WallLayer;
+    [SerializeField] private Transform WallCheck;
+
+    [Header("Ground Check")]
+    [SerializeField] Transform SpherePosition;
+    [SerializeField] float SphereRadius;
+    [SerializeField] LayerMask GroundMask;
+    Vector2 DashDirection;
 
     MovementState State = MovementState.Idle;
-    bool IsHoldingJump = false;
+    private bool IsHoldingJump = false;
+
+
     Vector2 MovementAxis;
     private void Awake()
     {
@@ -46,45 +65,90 @@ public class MovementScript : MonoBehaviour
     private void Update()
     {
         IsHoldingJump = Input.GetKey(KeyCode.Space);
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(IsGrounded && DashingCoroutine != null)
+        {
+            StopCoroutine(DashingCoroutine);
+            CanDash = true;
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded)
         {
             Jump();
         }
         if(Input.GetKeyDown(KeyCode.F))
         {
-
+            if(!CanDash) { return; }
+            DashingCoroutine = StartCoroutine(Dash());
         }
     }
 
+    private void FixedUpdate()
+    {        
+        UpdateAnimationState();
+        ForceMaxSpeed();
+        WallSlide();
+        if(IsDashing)
+        {
+            rb.velocity = DashDistance * DashDirection;
+            return;
+        }
+        rb.velocity = new Vector2(MovementAxis.x * MovementSpeed, rb.velocity.y);
+        FallingLogic();
+    }
+    IEnumerator Dash()
+    {
+        IsDashing = true;
+        CanDash = false;
+        DashDirection = MovementAxis.normalized;
+        yield return new WaitForSeconds(DashDuration);
+        IsDashing = false;
+        yield return new WaitForSeconds(DashCooldown);
+        CanDash = true;
+        yield return null;
+    }
+    bool IsGrounded
+    {
+        get { return Physics2D.OverlapCircle(SpherePosition.position, SphereRadius, GroundMask); }
+    }
+    bool IsWalled
+    {
+        get { return Physics2D.OverlapCircle(WallCheck.position, SphereRadius, WallLayer);}
+    }
     private void Jump()
     {
         rb.velocity = JumpSpeed * Vector2.up;
     }
 
-    private void FixedUpdate()
+    private void WallSlide()
     {
-        //rb.AddForce(Time.fixedDeltaTime * MovementSpeed * MovementAxis, ForceMode2D.Impulse);
-        rb.velocity = new Vector2(MovementAxis.x * MovementSpeed, rb.velocity.y);
-        ForceMaxSpeed();        
-        UpdateAnimationState();           
-        if(rb.velocity.y < 0)
+        if(!IsGrounded && IsWalled && MovementAxis.x != 0)
+        {
+            IsWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -WallSlidingSpeed, float.MaxValue));
+        }
+        else
+        {
+            IsWallSliding = false;
+        }
+    }
+
+    private void FallingLogic()
+    {
+        if (rb.velocity.y < 0)
         {
             rb.AddForce(Physics2D.gravity.y * FallMultiplier * Time.fixedDeltaTime * Vector2.up, ForceMode2D.Impulse);
-            //rb.velocity += Physics2D.gravity.y * (FallMultiplier - 1) * Time.deltaTime * Vector2.up;
         }
-        else if(rb.velocity.y > 0 && !IsHoldingJump)
+        else if (rb.velocity.y > 0 && !IsHoldingJump)
         {
             rb.AddForce(Physics2D.gravity.y * LowJumpMultiplier * Time.fixedDeltaTime * Vector2.up, ForceMode2D.Impulse);
-            //rb.velocity += Physics2D.gravity.y * (LowJumpMultiplier - 1) * Time.deltaTime * Vector2.up;
         }
     }
 
     private void ForceMaxSpeed()
     {
-        if (rb.velocity.x > MaxSpeed)
-            rb.velocity = new Vector2(MaxSpeed, rb.velocity.y);
-        else if(rb.velocity.x < -MaxSpeed)
-            rb.velocity = new Vector2(-MaxSpeed, rb.velocity.y);
+        if (rb.velocity.y > MaxYSpeed)
+            rb.velocity = new Vector2(rb.velocity.x, MaxYSpeed);
+        else if(rb.velocity.y < -MaxYSpeed)
+            rb.velocity = new Vector2(rb.velocity.x, -MaxYSpeed);
     }
 
     private void UpdateAnimationState()
@@ -92,12 +156,12 @@ public class MovementScript : MonoBehaviour
         if(rb.velocity.x > 0.01f)
         {
             State = MovementState.Walking;
-            SpriteRenderer.flipX = false;
+            transform.localScale = new Vector3(1f, transform.localScale.y, transform.localScale.z);
         }
         else if(rb.velocity.x < -0.01f)
         {
             State = MovementState.Walking;
-            SpriteRenderer.flipX = true;
+            transform.localScale = new Vector3(-1f, transform.localScale.y, transform.localScale.z);
         }
         else
             State = MovementState.Idle;
@@ -111,14 +175,27 @@ public class MovementScript : MonoBehaviour
             State = MovementState.Falling;
         }
 
+        if(IsWallSliding)
+            State = MovementState.WallSliding;
+
+        if(IsDashing)
+            State = MovementState.Dashing;
+
         Animator.SetInteger("State", (int)State);
     }
 
     private void OnMove(InputAction.CallbackContext context)
     {
-        MovementAxis = new Vector2(context.ReadValue<float>(), 0f);
+        MovementAxis = context.ReadValue<Vector2>();
     }
-    
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(SpherePosition.position, SphereRadius);
+        Gizmos.DrawWireSphere(WallCheck.position, SphereRadius);
+    }
+
     enum MovementState
     {
         Idle,
@@ -126,7 +203,7 @@ public class MovementScript : MonoBehaviour
         Jumping,
         Falling,
         Attacking,
-        Sliding,
+        WallSliding,
         Dashing
     }
 }
